0fb41c4d163a1926aeef035806fb3013f1a9fa21
Viacheslav Baranov <barash.2018@yandex.ru>
2025-05-22 09:22:20 +0300

    .env убран из индексации

--- src/app/ml/rag/issue_processor.py
+++ src/app/ml/rag/issue_processor.py
@@ -1,12 +1,13 @@
 
 from app.ml.ocr.ocr import process_photo
-from app.ml.rag.rag_faq import inference as faq_inference
+#from app.ml.rag.rag_faq import inference as faq_inference
+from app.ml.rag.rag_search import rag_service
 
 def process_issue(issue: dict) -> str:
     text = issue.get("text")
     photos = issue.get("photos")
     if photos is None:
-        return faq_inference(text)
+        return rag_service.get_answer(text)
     else:
         # search in Stubs
         return None
